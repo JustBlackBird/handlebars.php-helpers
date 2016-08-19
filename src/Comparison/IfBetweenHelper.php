fa6f4feea3d1c54e87fc1acc7aa315dd12dc3fe5
Dmitriy Simushev <simushevds@gmail.com>
2016-08-19 16:02:04 +0000

    Fix typo in ifBetween helper's docs

--- src/Comparison/IfBetweenHelper.php
+++ src/Comparison/IfBetweenHelper.php
@@ -16,12 +16,12 @@ use Handlebars\Helper as HelperInterface;
  * Conditional helper that checks if one value is between two another values.
  *
  * Borders of the interval are excluded from comparison. That's why in cases
- * where the value under comparision is equal to one of the borders the result of
- * "between" operator is false.
+ * where the value under comparision is equal to one of the borders the result
+ * of "between" operator is false.
  *
  * Example of usage:
  * ```handlebars
- *   {{#ifBetween value leftBorder rightBorder}
+ *   {{#ifBetween value leftBorder rightBorder}}
  *     The first argument is between borders.
  *   {{else}}
  *     The first argument is not between the borders.
